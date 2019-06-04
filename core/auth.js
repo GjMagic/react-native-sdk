0cd5e23dd41015d5bbb16a0e36c05c4141d423e7
gongjian <862635037@qq.com>
2019-06-04 08:59:52 +0800

    Update auth.js

--- core/auth.js
+++ core/auth.js
@@ -1,122 +1,152 @@
-/**
- * Created by buhe on 16/4/12.
- */
 import base64 from 'base-64';
 import CryptoJS from "crypto-js";
 import conf from "./conf.js";
 import parse from 'url-parse';
 
 function urlsafeBase64Encode(jsonFlags) {
-  var encoded = base64.encode(jsonFlags);
-  return base64ToUrlSafe(encoded);
+    var encoded = base64.encode(jsonFlags);
+    return base64ToUrlSafe(encoded);
 };
 
 function base64ToUrlSafe(v) {
-  return v.replace(/\//g, '_').replace(/\+/g, '-');
+    return v.replace(/\//g, '_').replace(/\+/g, '-');
 };
 
 function hmacSha1(encodedFlags, secretKey) {
-  var encoded = CryptoJS.HmacSHA1(encodedFlags, secretKey).toString(CryptoJS.enc.Base64);
-  return encoded;
+    var encoded = CryptoJS.HmacSHA1(encodedFlags, secretKey).toString(CryptoJS.enc.Base64);
+    return encoded;
 };
 
 function generateAccessToken(url, body) {
-  var u = parse(url, true);
+    var u = parse(url, true);
 
-  var path = u.pathname;
-  var access = path + '\n';
+    var path = u.pathname;
+    var access = path + '\n';
 
-  if (body) {
-    access += body;
-  }
+    if (body) {
+        access += body;
+    }
 
-  var digest = hmacSha1(access, conf.SECRET_KEY);
-  var safeDigest = base64ToUrlSafe(digest);
-  let token = 'QBox ' + conf.ACCESS_KEY + ':' + safeDigest;
-  //console.log(token);
-  return token;
+    var digest = hmacSha1(access, conf.SECRET_KEY);
+    var safeDigest = base64ToUrlSafe(digest);
+    let token = 'QBox ' + conf.ACCESS_KEY + ':' + safeDigest;
+    //console.log(token);
+    return token;
 };
 
-class PutPolicy2 {
-  constructor(putPolicyObj) {
-    if (typeof putPolicyObj !== 'object') {
-      return false;
+
+
+class Policy {
+    constructor(policy) {
+        if (typeof (policy) == "undefined") {
+        } else {
+            this.policy = policy;
+            if (typeof (policy.deadline) == "undefined" || policy.deadline == null) {
+                this.policy.deadline = 3600 + Math.floor(Date.now() / 1000);
+            }
+        }
     }
 
-    this.scope = putPolicyObj.scope || null;
-    this.expires = putPolicyObj.expires || 3600;
-    this.insertOnly = putPolicyObj.insertOnly || null;
+    _parse2Str(putPolicy) {
+        let str = "{";
+        let keys = Object.keys(putPolicy);
+        keys.forEach((key, i) => {
+            let value = putPolicy[key];
+            if (typeof (value) == "object") {
+                str = `${str}"${key}":`
+                str = `${str}"{`
+                Object.keys(value).forEach((key2) => {
+                    let value2 = value[key2];
+                    let re = /(\$\(.*?\))/g;
+                    if(re.test(value2)){
+                        str = `${str}\\\"${key2}\\\":${value2},`
+                    }else{
+                        str = `${str}\\\"${key2}\\\":"${value2}",`
+                    }
 
-    this.saveKey = putPolicyObj.saveKey || null;
-    this.endUser = putPolicyObj.endUser || null;
-
-    this.returnUrl = putPolicyObj.returnUrl || null;
-    this.returnBody = putPolicyObj.returnBody || null;
-
-    this.callbackUrl = putPolicyObj.callbackUrl || null;
-    this.callbackHost = putPolicyObj.callbackHost || null;
-    this.callbackBody = putPolicyObj.callbackBody || null;
-    this.callbackBodyType = putPolicyObj.callbackBodyType || null;
-
-    this.persistentOps = putPolicyObj.persistentOps || null;
-    this.persistentNotifyUrl = putPolicyObj.persistentNotifyUrl || null;
-    this.persistentPipeline = putPolicyObj.persistentPipeline || null;
-
-    this.fsizeLimit = putPolicyObj.fsizeLimit || null;
-
-    this.fsizeMin = putPolicyObj.fsizeMin || null;
-
-    this.detectMime = putPolicyObj.detectMime || null;
-
-    this.mimeLimit = putPolicyObj.mimeLimit || null;
-  }
-
-  token() {
-    var flags = this.getFlags();
-    var encodedFlags = urlsafeBase64Encode(JSON.stringify(flags));
-    var encoded = hmacSha1(encodedFlags, conf.SECRET_KEY);
-    var encodedSign = base64ToUrlSafe(encoded);
-    var uploadToken = conf.ACCESS_KEY + ':' + encodedSign + ':' + encodedFlags;
-    return uploadToken;
-  }
-
-  getFlags() {
-    var flags = {};
-    var attrs = ['scope', 'insertOnly', 'saveKey', 'endUser', 'returnUrl', 'returnBody', 'callbackUrl', 'callbackHost', 'callbackBody', 'callbackBodyType', 'callbackFetchKey', 'persistentOps', 'persistentNotifyUrl', 'persistentPipeline', 'fsizeLimit', 'fsizeMin', 'detectMime', 'mimeLimit'];
-
-    for (var i = attrs.length - 1; i >= 0; i--) {
-      if (this[attrs[i]] !== null) {
-        flags[attrs[i]] = this[attrs[i]];
-      }
+                })
+                console.log(keys.length + "::" + i)
+                if (i >= keys.length) {
+                    str = `${str.substring(0, str.length - 1)}}"`
+                }else{
+                    str = `${str.substring(0, str.length - 1)}}",`
+                }
+            }
+            else if (typeof (value) == "number") {
+                str = `${str}"${key}":${value},`
+            }
+            else if (typeof (value) == "string") {
+                str = `${str}"${key}":"${value}",`
+            }
+            else {
+                str = `${str}"${key}":"${value}",`
+            }
+        })
+        str = `${str.substring(0, str.length - 1)}}`;
+        return str;
     }
 
-    flags['deadline'] = this.expires + Math.floor(Date.now() / 1000);
 
-    return flags;
-  }
+    // _creatStr = (policy) => {
+    //   policy['deadline'] = this.expires + Math.floor(Date.now() / 1000);
+    //   let policyStr = JSON.stringify(policy);
+    // let re = /(\"\$\(.*?\)\")/g;
+    //   let newStr = policyStr.replace(re, (value) => {
+    //     return value.substring(1, value.length - 1);
+    //   })
+    //   return newStr;
+    // }
+
+    token = () => {
+        policStr = this._parse2Str(this.policy);
+        console.log("policStr", policStr);
+        var encodedPutPolicy = this._urlsafeBase64Encode(policStr);
+        console.log("encodedPutPolicy", encodedPutPolicy);
+        var sign = this._hmacSha1(encodedPutPolicy, conf.SECRET_KEY);
+        var encodedSign = this._base64ToUrlSafe(sign);
+        console.log("encodedSign", encodedSign);
+        var uploadToken = conf.ACCESS_KEY + ':' + encodedSign + ':' + encodedPutPolicy;
+        console.log("uploadToken", uploadToken);
+        return uploadToken;
+    }
+
+    _urlsafeBase64Encode = (jsonFlags) => {
+        var encoded = base64.encode(jsonFlags);
+        return base64ToUrlSafe(encoded);
+    };
+
+    _base64ToUrlSafe = (v) => {
+        return v.replace(/\//g, '_').replace(/\+/g, '-');
+    };
+
+    _hmacSha1 = (encodedFlags, secretKey) => {
+        var encoded = CryptoJS.HmacSHA1(encodedFlags, secretKey).toString(CryptoJS.enc.Base64);
+        return encoded;
+    };
+
 }
 
 class GetPolicy {
-  constructor(expires) {
-    this.expires = expires || 3600;
-  }
-
-  makeRequest(baseUrl) {
-    var deadline = this.expires + Math.floor(Date.now() / 1000);
-
-    if (baseUrl.indexOf('?') >= 0) {
-      baseUrl += '&e=';
-    } else {
-      baseUrl += '?e=';
+    constructor(expires) {
+        this.expires = expires || 3600;
     }
-    baseUrl += deadline;
 
-    var signature = hmacSha1(baseUrl, conf.SECRET_KEY);
-    var encodedSign = base64ToUrlSafe(signature);
-    var downloadToken = conf.ACCESS_KEY + ':' + encodedSign;
+    makeRequest(baseUrl) {
+        var deadline = this.expires + Math.floor(Date.now() / 1000);
 
-    return baseUrl + '&token=' + downloadToken;
-  }
+        if (baseUrl.indexOf('?') >= 0) {
+            baseUrl += '&e=';
+        } else {
+            baseUrl += '?e=';
+        }
+        baseUrl += deadline;
+
+        var signature = hmacSha1(baseUrl, conf.SECRET_KEY);
+        var encodedSign = base64ToUrlSafe(signature);
+        var downloadToken = conf.ACCESS_KEY + ':' + encodedSign;
+
+        return baseUrl + '&token=' + downloadToken;
+    }
 }
 
-export default {urlsafeBase64Encode,generateAccessToken,PutPolicy2,GetPolicy}
+export default { urlsafeBase64Encode, generateAccessToken, Policy, GetPolicy }
